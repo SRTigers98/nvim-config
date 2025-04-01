5c3b9d7158a7db42c88f059e36bb3d0fed450f87
Benjamin Eder <eder.ben01@gmail.com>
2025-04-01 08:16:42 +0200

    migrate vim.sign_define to vim.diagnostic

--- lua/luna/plugins/lsp/lspconfig.lua
+++ lua/luna/plugins/lsp/lspconfig.lua
@@ -70,12 +70,23 @@ return {
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
     -- Change the Diagnostic symbols in the sign column (gutter)
-    -- (not in youtube nvim video)
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+    -- (not in youtube nvim video, migrated to vim.diagnostic.config)
+    vim.diagnostic.config {
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.INFO] = " ",
+          [vim.diagnostic.severity.HINT] = "󰠠 ",
+        },
+        numhl = {
+          [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+          [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+          [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+          [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+        },
+      },
+    }
 
     mason_lspconfig.setup_handlers {
       -- default handler for installed servers
