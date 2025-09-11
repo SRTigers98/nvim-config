b4e3d41cf2036c8a2c3e8a1565d668117fa63eda
Benjamin Eder <eder.ben01@gmail.com>
2025-09-11 09:24:24 +0200

    add flash.nvim
    lazy sync

---
+++ lua/luna/plugins/flash.lua
@@ -0,0 +1,30 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    label = {
+      rainbow = {
+        enabled = true,
+      },
+    },
+    modes = {
+      search = {
+        enabled = true,
+      },
+      char = {
+        jump_labels = true,
+      },
+    },
+  },
+  keys = {
+    { "<leader>js", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<leader>jS", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<leader>jr", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    {
+      "<leader>jR",
+      mode = { "o", "x" },
+      function() require("flash").treesitter_search() end,
+      desc = "Treesitter Search",
+    },
+  },
+}
